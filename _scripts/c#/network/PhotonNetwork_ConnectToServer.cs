6f97543941e5bc274f022efe2d2a331148940e41
InnerTiger71 <inner.tiger71@gmail.com>
2015-10-31 00:51:57 +0300

    0.1a
    First

---
+++ _scripts/c#/network/PhotonNetwork_ConnectToServer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PhotonNetwork_ConnectToServer : Photon.MonoBehaviour {
+
+    // Use this for initialization
+    void Start() {
+
+        PhotonNetwork.ConnectUsingSettings("0.1a");
+    }
+
+    void OnJoinedLobby()
+    {
+        PhotonNetwork.JoinOrCreateRoom("StandartLobby", new RoomOptions(), TypedLobby.Default);
+    }
+
+}
